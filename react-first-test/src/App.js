32c6ea4b806d218fdf64dffbd4a0871b4cac18e2
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2019-01-18 16:34:41 -0200

    need fix enzymer-adapter bug

---
+++ react-first-test/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CheckboxWithLabel from "./CheckboxWithLabel";
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CheckboxWithLabel />
+      </div>
+    );
+  }
+}
+
+export default App;
